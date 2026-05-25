2a69ff055bf399f35b4758888a4046213c0ba03d
tithy-ghosh <sgt.tithy.717@gmail.com>
2026-05-25 12:49:37 +0600

    Added the zod validation, sign-in, sign-up feature using next-auth

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+    const { data: session } = useSession()
+    if(session) {
+        return (
+            <>
+            Signed in as {session.user.email} <br /> 
+            <button onClick={() => signOut()}>Sign Out</button>
+            </>
+        )
+    }
+} 
